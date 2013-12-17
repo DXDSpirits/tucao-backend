5f5bb84c1ed19dd46dc117c9f1e3776ef89544ef
Xindong Ding <dxd.spirits@gmail.com>
2013-12-17 23:33:51 +0800

    override form styles on admin page; add tags filter to tweets list

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.forms import Textarea, SelectMultiple
 from tweets.models import *
 
 class TagAdmin(admin.ModelAdmin):
@@ -6,6 +7,11 @@ class TagAdmin(admin.ModelAdmin):
 
 class TweetAdmin(admin.ModelAdmin):
     list_display = ['id', 'content']
+    list_filter = ('tags',)
+    formfield_overrides = {
+        models.CharField: {'widget': Textarea(attrs={'rows': 4, 'cols': 50})},
+        models.ManyToManyField: {'widget': SelectMultiple(attrs={'style': 'height: 20em; min-width: 10em;'})},
+    }
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Tweet, TweetAdmin)
